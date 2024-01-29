f6d57cd08ca3969ddd16e54a605f5010c168526f
jaswa ramesh <jaswant.r@kalvium.community>
2024-01-29 17:03:37 +0530

    commit

---
+++ probooks/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from "axios"
+
+function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get("https://reactnd-books-api.udacity.com/books",{ headers: { 'Authorization': 'whatever-you-want' }})
+    .then((res) => setData(res.data.books))
+    .catch((error) => console.error(error))
+  },[])
+
+  return (
+    <>
+      {
+        data.map(items => {
+          return(
+            <div>
+              <h1>{items.title}</h1>
+              
+              <p>{items.description}</p>
+              <img src={items.imageLinks.thumbnail} alt="" />
+              {items.authors.map((ele, i) => {
+                return <p key={i}>{ele}</p>
+              })}
+            </div>
+           
+          )
+        })
+      }
+    </>
+  )
+}
+
+export default App
